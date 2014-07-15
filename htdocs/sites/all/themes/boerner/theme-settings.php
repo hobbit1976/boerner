ba37a5d33fcf909c2ed14a304473e1a43255d97e
hkramer <holger.kramer@arts.aero>
2014-07-15 15:12:27 +0200

    Boerner Truck Theme angelegt

--- htdocs/sites/all/themes/boerner/theme-settings.php
+++ htdocs/sites/all/themes/boerner/theme-settings.php
@@ -7,4 +7,4 @@
  * @param $form_state
  *   The form state.
  */
-function STARTER_form_system_theme_settings_alter(&$form, &$form_state) {}
+function boerner_form_system_theme_settings_alter(&$form, &$form_state) {}
